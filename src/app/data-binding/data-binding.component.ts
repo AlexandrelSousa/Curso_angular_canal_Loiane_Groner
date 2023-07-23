c8caa38102b2ae63106150376c5addf6a8b82b4f
alexandrelsousa <alexandrelsousa@outlook.com>
2023-07-23 18:06:46 -0300

    Aprendendo sobre os event blindings e aplicando alguns deles no codigo.

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -11,11 +11,27 @@ export class DataBindingComponent {
   cursoAngular: boolean = true;
   urlImage: String = 'http://lorempixel.com.br/400/200';
 
+  valorAtual: String = '';
+  valorSalvo:any = '';
+  isMouseOver: boolean = false;
+
   getValor(){
     return 1;
   }
   getCurtirCurso(){
     return true;
+  }
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor:any){
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
 }
